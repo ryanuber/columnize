da1ef32ab26acc19d36b7382ef853892a544a983
Ryan Uber <ru@ryanuber.com>
2014-08-25 16:21:04 -0700

    Attach getStringFormat to Config to avoid explicitly passing config values to it

--- columnize.go
+++ columnize.go
@@ -57,15 +57,16 @@ func getWidthsFromLines(lines []string, delim string) []int {
 // Given a set of column widths and the number of columns in the current line,
 // returns a sprintf-style format string which can be used to print output
 // aligned properly with other lines using the same widths set.
-func getStringFormat(widths []int, columns int, space string) string {
-	var stringfmt string
+func (c *Config) getStringFormat(widths []int, columns int) string {
+	// Start with the prefix, if any was given.
+	stringfmt := c.Prefix
 
 	// Create the format string from the discovered widths
 	for i := 0; i < columns && i < len(widths); i++ {
 		if i == columns-1 {
 			stringfmt += "%s\n"
 		} else {
-			stringfmt += fmt.Sprintf("%%-%ds%s", widths[i], space)
+			stringfmt += fmt.Sprintf("%%-%ds%s", widths[i], c.Glue)
 		}
 	}
 	return stringfmt
@@ -81,8 +82,8 @@ func Format(lines []string, config *Config) string {
 	// Create the formatted output using the format string
 	for _, line := range lines {
 		elems := getElementsFromLine(line, config.Delim)
-		stringfmt := getStringFormat(widths, len(elems), config.Glue)
-		result += fmt.Sprintf(config.Prefix+stringfmt, elems...)
+		stringfmt := config.getStringFormat(widths, len(elems))
+		result += fmt.Sprintf(stringfmt, elems...)
 	}
 
 	// Remove trailing newline without removing leading/trailing space
